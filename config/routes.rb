a5d1386686ce76122cb503cc067ac2a4a7e83d5d
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-23 16:01:48 -0600

    Feat: add an alias for the /posts/:id route related tot he show action in the post controller.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/posts/:id", to: "posts#show"
+  get "/posts/:id", to: "posts#show", as: 'post'
 
   # Defines the root path route ("/")
   root "posts#index"
