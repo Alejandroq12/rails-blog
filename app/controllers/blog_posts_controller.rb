5ae02c7bb0fc7eaf66ee316fd0a95ef36a33a6d6
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-24 08:21:54 -0600

    Feat: add logic to the create action.

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -14,5 +14,17 @@ class BlogPostsController < ApplicationController
   end
 
   def create
+    @blog_post = BlogPost.new(blog_post_params)
+    if @blog_post.save
+      redirect_to @blog_post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :body)
   end
 end
