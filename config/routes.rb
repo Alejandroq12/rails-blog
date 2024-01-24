174d71052a1976bcf220e28c4c4ab6f45087ba66
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-24 15:05:08 -0600

    Feat: use the resources method to declare standard RESTful routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
+  resources :blog_posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/blog_posts/new", to: "blog_posts#new", as: 'new_blog_post'
-  get "/blog_posts/:id", to: "blog_posts#show", as: 'blog_post'
-  patch "/blog_posts/:id", to: "blog_posts#update"
-  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: 'edit_blog_post'
-  post "/blog_posts", to: "blog_posts#create", as: 'blog_posts'
+  # get "/blog_posts/new", to: "blog_posts#new", as: 'new_blog_post'
+  # get "/blog_posts/:id", to: "blog_posts#show", as: 'blog_post'
+  # patch "/blog_posts/:id", to: "blog_posts#update"
+  # delete "/blog_posts/:id", to: "blog_posts#destroy"
+  # get "/blog_posts/:id/edit", to: "blog_posts#edit", as: 'edit_blog_post'
+  # post "/blog_posts", to: "blog_posts#create", as: 'blog_posts'
 
   # Defines the root path route ("/")
   root "blog_posts#index"
