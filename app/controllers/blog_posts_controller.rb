78c32012470d5d27c27f9a47f14c20f890c9058e
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-24 15:06:49 -0600

    Refactor: blog_post_controller to use a helper method to have clean code and create the destroy action.

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,13 +1,11 @@
 class BlogPostsController < ApplicationController
+  before_action :set_blog_post, except: %i[index new create]
+
   def index
     @blog_posts = BlogPost.all
   end
 
-  def show
-    @blog_post = BlogPost.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    redirect_to root_path
-  end
+  def show; end
 
   def new
     @blog_post = BlogPost.new
@@ -22,12 +20,9 @@ class BlogPostsController < ApplicationController
     end
   end
 
-  def edit
-    @blog_post = BlogPost.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @blog_post = BlogPost.find(params[:id])
     if @blog_post.update(blog_post_params)
       redirect_to @blog_post
     else
@@ -35,9 +30,20 @@ class BlogPostsController < ApplicationController
     end
   end
 
+  def destroy
+    @blog_post.destroy
+    redirect_to root_path
+  end
+
   private
 
   def blog_post_params
     params.require(:blog_post).permit(:title, :body)
+  end
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
   end
 end
